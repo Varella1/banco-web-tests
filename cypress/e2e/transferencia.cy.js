77b4accbb33e29fa48cf95b36728120a950bd88b
Felipe <felipevarella1@gmail.com>
2025-09-12 11:51:53 -0300

    Implementando comandos customizáveis para Login, Tranferências e Commons

---
+++ cypress/e2e/transferencia.cy.js
@@ -0,0 +1,23 @@
+describe('Transferencias', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.fazerLoginComCredenciaisValidas()
+    })
+    it('Deve transferir quando informo dados e valor válido', () => {
+        //Act
+        cy.realizarTransferencia('João da Silva','Maria Oliveira','100')
+        
+        //Assert
+        cy.verificarMensagemNoToast('Transferência realizada!')
+    })
+
+    it('Deve apresentar erro quando tentar transferir mais que 5 mil sem o token', () => {
+        //Act
+        cy.realizarTransferencia('Maria Oliveira','João da Silva','6000')
+        
+        //Assert
+        cy.verificarMensagemNoToast('Autenticação necessária para transferências acima de R$5.000,00.')
+    })
+        
+        
+})
